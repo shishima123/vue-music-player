3280f2c2f02126a7563fdc8e4bfd6830132042f6
Phuoc Nguyen <shishima21@gmail.com>
2023-04-04 11:05:55 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
   publicPath:
     process.env.NODE_ENV === "production"
       ? "/vue-music-player/" // Thay tên repository của các bạn vào đây nhé
-      : "/"
+      : "/",
+  pwa: {
+    name: "Listening Player" // <---- this is PWA name
+  }
 };
