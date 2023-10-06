acc31aedf00a815845997551f23abe53d0604623
Trung Vo <t.vo@asoljp.com>
2023-10-06 13:44:57 +0700

    deploy

---
+++ precache-manifest.502bf05b10f82ce190fc8c2f88f7266e.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8ceb29e9291eec8750",
+    "url": "/vue-music-player/css/app.6a40f18c.css"
+  },
+  {
+    "revision": "7721e95f205e20212ef6",
+    "url": "/vue-music-player/css/chunk-vendors.a8dfd12e.css"
+  },
+  {
+    "revision": "17aa932146899abcde5170a5410a55d3",
+    "url": "/vue-music-player/img/cover.17aa9321.jpg"
+  },
+  {
+    "revision": "ae53bbdc28f6d658d7b39078bad1dabf",
+    "url": "/vue-music-player/index.html"
+  },
+  {
+    "revision": "8e8ceb29e9291eec8750",
+    "url": "/vue-music-player/js/app.108b92db.js"
+  },
+  {
+    "revision": "7721e95f205e20212ef6",
+    "url": "/vue-music-player/js/chunk-vendors.91fd80f5.js"
+  },
+  {
+    "revision": "5baab65f7645236dc884ee09ba9c995f",
+    "url": "/vue-music-player/logo.png"
+  },
+  {
+    "revision": "1dcc59f83b0361c927a3e64bb6fc8d74",
+    "url": "/vue-music-player/manifest.json"
+  },
+  {
+    "revision": "1c57b9ff4e8a472720153fec57724ea4",
+    "url": "/vue-music-player/media/Unit 1 A Picnic by the River.1c57b9ff.mp3"
+  },
+  {
+    "revision": "8cc515b0b4eb9914e8e9946115b7aa40",
+    "url": "/vue-music-player/media/Unit 10 A New Camera.8cc515b0.mp3"
+  },
+  {
+    "revision": "94cc10b837797236ff006c0c29eb4388",
+    "url": "/vue-music-player/media/Unit 11 Gold.94cc10b8.mp3"
+  },
+  {
+    "revision": "db6462db68f8241ea7b0c97fd91b7cd4",
+    "url": "/vue-music-player/media/Unit 12 The Summer Music Festival.db6462db.mp3"
+  },
+  {
+    "revision": "d020755dc4e8254c661693c9cdf0a2d2",
+    "url": "/vue-music-player/media/Unit 13 The School Play.d020755d.mp3"
+  },
+  {
+    "revision": "44ffaea781c1817868ac7663f7738002",
+    "url": "/vue-music-player/media/Unit 14 Ballet Class.44ffaea7.mp3"
+  },
+  {
+    "revision": "ad80ff6416862dd662e1e7ffa7fbd148",
+    "url": "/vue-music-player/media/Unit 15 Monet’s Garden.ad80ff64.mp3"
+  },
+  {
+    "revision": "817ad4d2e4ab87472a1a3e5d6115ec60",
+    "url": "/vue-music-player/media/Unit 16 A Haunted House.817ad4d2.mp3"
+  },
+  {
+    "revision": "0bc68f1c110d8f92476dcd297570a7f3",
+    "url": "/vue-music-player/media/Unit 17 A Big Screen TV.0bc68f1c.mp3"
+  },
+  {
+    "revision": "751915bf04b685c1b56012f23444570e",
+    "url": "/vue-music-player/media/Unit 18 He’s Famous.751915bf.mp3"
+  },
+  {
+    "revision": "b5edcc19b0868811f9db3869c5db02b5",
+    "url": "/vue-music-player/media/Unit 19 A Housewarming Party.b5edcc19.mp3"
+  },
+  {
+    "revision": "f9bb0cfaf3dbb28412bde6eca62e3474",
+    "url": "/vue-music-player/media/Unit 2 Let’s Recycle.f9bb0cfa.mp3"
+  },
+  {
+    "revision": "c5f8140aaa241fa972f120596aeb0e1b",
+    "url": "/vue-music-player/media/Unit 20 Making Music.c5f8140a.mp3"
+  },
+  {
+    "revision": "895ba7c611ea009bda2f8f84b462026d",
+    "url": "/vue-music-player/media/Unit 21 Where Is Your Homework.895ba7c6.mp3"
+  },
+  {
+    "revision": "ffad429c9715bd67df238cb4c169e2f9",
+    "url": "/vue-music-player/media/Unit 22 After School Activities.ffad429c.mp3"
+  },
+  {
+    "revision": "e8b5a6e59a618a68a4073259fc3b30f6",
+    "url": "/vue-music-player/media/Unit 23 Arts and Crafts Class.e8b5a6e5.mp3"
+  },
+  {
+    "revision": "79e4df645bce30fbb18bce417846e0f6",
+    "url": "/vue-music-player/media/Unit 24 Grandfather’s Birthday.79e4df64.mp3"
+  },
+  {
+    "revision": "845f43e9d8b8ac9fad614d9140cda84a",
+    "url": "/vue-music-player/media/Unit 25 The Lesson.845f43e9.mp3"
+  },
+  {
+    "revision": "5c3ed1861c481c77a44f897703bb4e8e",
+    "url": "/vue-music-player/media/Unit 26 A Problem at the Office.5c3ed186.mp3"
+  },
+  {
+    "revision": "c8df0385cddf224d1d6ee377de6a7140",
+    "url": "/vue-music-player/media/Unit 27 My Future Job.c8df0385.mp3"
+  },
+  {
+    "revision": "44f624512db7121a772d5fcd63b8f66f",
+    "url": "/vue-music-player/media/Unit 28 Shopping.44f62451.mp3"
+  },
+  {
+    "revision": "807efe61aca52f04ef583b810524fe85",
+    "url": "/vue-music-player/media/Unit 29 A Part-time Job.807efe61.mp3"
+  },
+  {
+    "revision": "b839a695ac059aa032016e8faa4d4d24",
+    "url": "/vue-music-player/media/Unit 3 Growing Roses.b839a695.mp3"
+  },
+  {
+    "revision": "bd4c830bd541d085f8eab27a92aa1320",
+    "url": "/vue-music-player/media/Unit 30 Teamwork.bd4c830b.mp3"
+  },
+  {
+    "revision": "eb760359ccb52c8342cbd34839574642",
+    "url": "/vue-music-player/media/Unit 31 The Marathon.eb760359.mp3"
+  },
+  {
+    "revision": "749a2c3c4005c7d72d06f9624b3e21fc",
+    "url": "/vue-music-player/media/Unit 32 Racquetball.749a2c3c.mp3"
+  },
+  {
+    "revision": "7729a3ed9d6e231d821bb4607dbd2220",
+    "url": "/vue-music-player/media/Unit 33 Summer Sports.7729a3ed.mp3"
+  },
+  {
+    "revision": "ed7efa8a1147c61f1e6e37f88d2f66c4",
+    "url": "/vue-music-player/media/Unit 34 Inline Skating.ed7efa8a.mp3"
+  },
+  {
+    "revision": "845f0580c4621ab0b10e0abc172526b5",
+    "url": "/vue-music-player/media/Unit 35 The Olympics.845f0580.mp3"
+  },
+  {
+    "revision": "49e468ac47fa308d0b3e7df66beadeea",
+    "url": "/vue-music-player/media/Unit 36 A Bicycle Trip.49e468ac.mp3"
+  },
+  {
+    "revision": "b0ec0b74b1c90bb977aa860be33e6a3f",
+    "url": "/vue-music-player/media/Unit 37 Driving Down South.b0ec0b74.mp3"
+  },
+  {
+    "revision": "edc33896f9f6925a91937de0dcba04cf",
+    "url": "/vue-music-player/media/Unit 38 The Travel Agent.edc33896.mp3"
+  },
+  {
+    "revision": "4d04cb3ce0595028e6eb54fe544a6a46",
+    "url": "/vue-music-player/media/Unit 39 Taking the Train.4d04cb3c.mp3"
+  },
+  {
+    "revision": "9d35b12ae2cc85db00a79c90e636ae38",
+    "url": "/vue-music-player/media/Unit 4 Bird Watching.9d35b12a.mp3"
+  },
+  {
+    "revision": "b68544f88bdb5d5f4b0e20f5c92761aa",
+    "url": "/vue-music-player/media/Unit 40 Go on a Cruise.b68544f8.mp3"
+  },
+  {
+    "revision": "2911d243a3d4122a125441ec61c12ae7",
+    "url": "/vue-music-player/media/Unit 5 Trees in the Forest.2911d243.mp3"
+  },
+  {
+    "revision": "0bb03fa608941985d6bc858972905527",
+    "url": "/vue-music-player/media/Unit 6 An Easy Way to Shop.0bb03fa6.mp3"
+  },
+  {
+    "revision": "09f2ffc645307d9d2dd9946065988b81",
+    "url": "/vue-music-player/media/Unit 7 Cell Phone Messages.09f2ffc6.mp3"
+  },
+  {
+    "revision": "4ca5073617c017f1c55f127a1d5fa55d",
+    "url": "/vue-music-player/media/Unit 8 Bubbles in Boiling Water.4ca50736.mp3"
+  },
+  {
+    "revision": "7daab5e1ae7e36854e912ee717f3fec1",
+    "url": "/vue-music-player/media/Unit 9 The Blue Sky.7daab5e1.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-music-player/robots.txt"
+  }
+]);
